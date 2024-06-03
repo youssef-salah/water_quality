178f9980d86564fe68505429f02fd6c898a6e1dc
youssef-salah <77450576+youssef-salah@users.noreply.github.com>
2024-06-04 00:42:51 +0300

    Add files via upload

---
+++ water_quality.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, jsonify, make_response
+import joblib
+import pandas as pd
+
+app = Flask(__name__)
+regr_energy = joblib.load('Water_Quality.joblib')
+
+@app.route('/wind', methods=['POST', 'GET'])
+def handle_data():
+   try :
+
+      if request.method == 'POST':
+         jdata = request.json
+         jdata_df = pd.DataFrame([jdata])
+         
+      elif request.method == 'GET':
+   # Get parameters from query string
+         params = ["ph", "Hardness","Solids"
+                   ,"Chloramines" , "Sulfate" ,
+                     "Conductivity" , "Organic_carbon" ,
+                     "Trihalomethanes" , " Turbidity"]
+         jdata = {param : float (request.args.get(param , 0)) for param in params}
+         jdata_df = pd.DataFrame([jdata])
+# Create DataFrame with the received data
+      
+      predicted_water_quality = regr_energy.predict(jdata_df)
+      predicted_water_quality = predicted_water_quality.tolist()
+
+# Create response with CORS headers
+      response = make_response(jsonify({'predicted_wind_energy': predicted_water_quality[0]}))
+      response.headers['Access-Control-Allow-Origin'] = '*'
+      response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+      response.headers['Access-Control-Allow-Methods'] = 'GET, POST'
+
+      return response
+
+   except Exception as e :
+      response = make_response(jsonify({'error' : str(e)}), 400)
+
+      return response
+
+if __name__ == '__main__':
+ app.run(debug=True, host='0.0.0.0', port=3000)
